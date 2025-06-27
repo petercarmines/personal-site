d3fdcc0cdeee339f531fc3a48e68873cd0248b96
pete <petercarmines@gmail.com>
2025-06-27 18:46:49 -0400

    test

--- src/components/NameAnimation.js
+++ src/components/NameAnimation.js
@@ -1,16 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
 const NameAnimation = () => {
   const name = 'Peter Carmines';
-  const [movingIndex, setMovingIndex] = useState(null); // no letter moves initially
+
+  // State: keep track of which letters are moving
+  const [movingLetters, setMovingLetters] = useState(new Set());
+
+  // Persistent random motion values for each letter
+  const randomMotionValues = useMemo(() => {
+    const values = {};
+    name.split('').forEach((_, index) => {
+      values[index] = {
+        '--rand-x1': Math.random() * 2 - 1,
+        '--rand-y1': Math.random() * 2 - 1,
+        '--rand-x2': Math.random() * 2 - 1,
+        '--rand-y2': Math.random() * 2 - 1,
+        '--rand-x3': Math.random() * 2 - 1,
+        '--rand-y3': Math.random() * 2 - 1,
+        '--rand-x4': Math.random() * 2 - 1,
+        '--rand-y4': Math.random() * 2 - 1,
+      };
+    });
+    return values;
+  }, [name]);
 
   const handleClick = (index) => {
-    // If clicking the already moving letter, stop all
-    if (movingIndex === index) {
-      setMovingIndex(null);
-    } else {
-      setMovingIndex(index);
-    }
+    setMovingLetters((prev) => {
+      const newSet = new Set(prev);
+      if (newSet.has(index)) {
+        newSet.delete(index); // stop animation if clicked again
+      } else {
+        newSet.add(index); // start animation
+      }
+      return newSet;
+    });
   };
 
   return (
@@ -18,19 +41,10 @@ const NameAnimation = () => {
       {name.split('').map((letter, index) => (
         <span
           key={index}
-          className={`letter ${movingIndex === index ? 'moving' : ''}`}
+          className={`letter ${movingLetters.has(index) ? 'moving' : ''}`}
           onClick={() => handleClick(index)}
           data-testid={`letter-${index}`}
-          style={{
-            '--rand-x1': Math.random() * 2 - 1,
-            '--rand-y1': Math.random() * 2 - 1,
-            '--rand-x2': Math.random() * 2 - 1,
-            '--rand-y2': Math.random() * 2 - 1,
-            '--rand-x3': Math.random() * 2 - 1,
-            '--rand-y3': Math.random() * 2 - 1,
-            '--rand-x4': Math.random() * 2 - 1,
-            '--rand-y4': Math.random() * 2 - 1,
-          }}
+          style={randomMotionValues[index]}
         >
           {letter === ' ' ? '\u00A0' : letter}
         </span>
